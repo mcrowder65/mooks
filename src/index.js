6a52251cac82d67ca1580bb47ee56c2daff958dd
Matt Crowder <mcrowder65@gmail.com>
2019-03-20 22:23:52 -0400

    need to add docs

--- src/index.js
+++ src/index.js
@@ -3,5 +3,21 @@ import { useCurrentState } from "./use-current-state";
 import { getElements } from "./get-elements";
 import { useLoader } from "./use-loader";
 import { sleep } from "./sleep";
+import { compose } from "./compose";
+import DefaultValue from "./default-value";
+import LoaderButton from "./loader-button";
+import LoaderCard from "./loader-card";
+import Login from "./login";
 
-export { useArrowKeyListener, useCurrentState, getElements, useLoader, sleep };
+export {
+  useArrowKeyListener,
+  useCurrentState,
+  getElements,
+  useLoader,
+  sleep,
+  compose,
+  DefaultValue,
+  LoaderButton,
+  LoaderCard,
+  Login,
+};

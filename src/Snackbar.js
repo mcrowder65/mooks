2caa686d61baa3bdf79c466101bbf69872868c21
Matt Crowder <mcrowder65@gmail.com>
2019-03-25 21:36:35 -0400

    use hooks for snackbar provider

--- src/Snackbar.js
+++ src/Snackbar.js
@@ -2,74 +2,65 @@ import React from "react";
 import Snackbar from "@material-ui/core/Snackbar";
 
 const SnackbarContext = React.createContext();
-export class SnackbarProvider extends React.Component {
-  queue = [];
-
-  state = {
-    open: false,
-    messageInfo: {},
-  };
-
-  handleClick = (message) => {
-    this.queue.push({
-      message,
-      key: new Date().getTime(),
-    });
-
-    if (this.state.open) {
-      // immediately begin dismissing current message
-      // to start showing new one
-      this.setState({ open: false });
-    } else {
-      this.processQueue();
-    }
-  };
-
-  processQueue = () => {
-    if (this.queue.length > 0) {
-      this.setState({
-        messageInfo: this.queue.shift(),
-        open: true,
+export const SnackbarProvider = (() => {
+  const queue = [];
+  return (props) => {
+    const [open, setOpen] = React.useState(false);
+    const [messageInfo, setMessageInfo] = React.useState({});
+    const processQueue = () => {
+      if (queue.length > 0) {
+        setMessageInfo(queue.shift());
+        setOpen(true);
+      }
+    };
+    const handleClick = (message) => {
+      queue.push({
+        message,
+        key: new Date().getTime(),
       });
-    }
-  };
 
-  handleClose = (event, reason) => {
-    if (reason === "clickaway") {
-      return;
-    }
-    this.setState({ open: false });
-  };
+      if (open) {
+        // immediately begin dismissing current message
+        // to start showing new one
+        setOpen(false);
+      } else {
+        processQueue();
+      }
+    };
 
-  handleExited = () => {
-    this.processQueue();
-  };
+    const handleClose = (event, reason) => {
+      if (reason === "clickaway") {
+        return;
+      }
+      setOpen(false);
+    };
 
-  render() {
-    const { messageInfo } = this.state;
+    const handleExited = () => {
+      processQueue();
+    };
     return (
-      <SnackbarContext.Provider value={{ addMessage: this.handleClick }}>
+      <SnackbarContext.Provider value={{ addMessage: handleClick }}>
         <Snackbar
           key={messageInfo.key}
           anchorOrigin={{
             vertical: "bottom",
             horizontal: "left",
           }}
-          open={this.state.open}
+          open={open}
           autoHideDuration={3000}
-          onClose={this.handleClose}
-          onExited={this.handleExited}
+          onClose={handleClose}
+          onExited={handleExited}
           ContentProps={{
             "aria-describedby": "message-id",
           }}
           message={<span id="message-id">{messageInfo.message}</span>}
         />
 
-        {this.props.children}
+        {props.children}
       </SnackbarContext.Provider>
     );
-  }
-}
+  };
+})();
 
 export function useSnackbar() {
   return React.useContext(SnackbarContext);

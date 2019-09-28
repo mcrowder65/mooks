0830966c9452d77c20542efcff91ecc8c47134be
Matt Crowder <mcrowder65@gmail.com>
2019-09-28 13:23:01 -0400

    added login and signup snaps

--- src/__tests__/Signup.test.js
+++ src/__tests__/Signup.test.js
@@ -1,7 +1,16 @@
 import React from "react";
 import { render } from "../../test/test-utils";
 import Signup from "../Signup";
+import markdown from "../Signup.md";
 
 test("that it renders without errors", () => {
   render(<Signup />);
+});
+
+const scope = { Signup };
+
+describe("it matches the visual snapshot", () => {
+  describe("it matches the visual snapshot", () => {
+    expect({ markdown, scope }).toMatchVisualSnapshot();
+  });
 });
--- test/config.js
+++ test/config.js
@@ -1,3 +1,19 @@
 require("jest-dom/extend-expect");
 require("babel-polyfill");
 require("react-testing-library/cleanup-after-each");
+const { configure, toMatchVisualSnapshot } = require("sosia");
+const { PuppeteerBrowserTarget } = require("sosia-puppeteer");
+const { MarkdownSource } = require("sosia-markdown");
+
+expect.extend({ toMatchVisualSnapshot });
+configure({
+  targets: {
+    "chrome-desktop": new PuppeteerBrowserTarget({
+      width: 1024,
+      height: 768,
+    }),
+  },
+  sources: {
+    documentation: new MarkdownSource(),
+  },
+});
